e6889ca6185d0bbcfa8d9cbf95aa8469dd0284fa
Larmesdesang <85700200+GeplLoL@users.noreply.github.com>
2023-01-30 02:54:32 +0300

    Update PythonApplication3.py

--- PythonApplication3/PythonApplication3.py
+++ PythonApplication3/PythonApplication3.py
@@ -1,3 +1,4 @@
+#from random import *
 #lis=[]
 #1
 #for i in range(5):
@@ -5,7 +6,7 @@
 #    lis.append(n) #lisa lisa
 #lis.sort() #lisa sort
 #print(lis) 
-#print("Last name ",n) #lõpp list
+#print("Last name ",n) #lÃµpp list
 #2
 #student = ["Juhan","Kati","Maarja","Mario","Mati"]
 #print(student)
@@ -15,15 +16,30 @@
 #student.insert(x-1,y) #lisa insert
 #print(student)
 #3
-try:
-    student = ["Juhan","Kati","Mario","Mario","Mati","Mati"]
-    student.sort()
-    for x in range(0,4):
-        print(x)
-        for i in range(0,4):
-            print(i)
-            if x!=0!=i and student[x]==student[i]:
-                student.pop(i)
-    print(student)
-except:
-    print(x)
+#opilased = ["Juhan","Kati","Mario","Mario","Mati"]
+#x = []
+#for y in opilased:
+#    if y not in x:
+#        x.append(y)
+#print(x)
+#4
+#vanus = randint(1,100)
+#vanus2 = randint(1,100)
+#vanus3 = randint(1,100)
+#x=[vanus, vanus2, vanus3]
+#print(max(x))
+#print(min(x))
+#print((vanus+vanus2+vanus3)/3)
+#print(vanus+vanus2+vanus3)
+#5
+#x=[1,2,3]
+#y="*"
+#for i in range(0,x[0]):
+#    print(y, end="")
+#print("")
+#for i in range(0,x[1]):
+#    print(y, end="")
+#print("")
+#for i in range(0,x[2]):
+#    print(y, end="")
+#print("")
