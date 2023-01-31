a3eea22b89e5f67d663c31afe7d1ed4c80665b3e
Larmesdesang <85700200+GeplLoL@users.noreply.github.com>
2023-02-01 02:17:22 +0300

    Update PythonApplication3.py

--- PythonApplication3/PythonApplication3.py
+++ PythonApplication3/PythonApplication3.py
@@ -43,7 +43,6 @@
 #for i in range(0,x[2]):
 #    print(y, end="")
 #print("")
-from random import *
 #opilased = ["Juhan","Kati","Mario","Mario","Mati","Juhan"]
 #nimi = input("Mis nimi on otsida: ")
 
@@ -58,10 +57,46 @@ from random import *
 #        print(opilased.index(nimi,0, 5)+1)
 
 
-
-x=[1,2,3]
-kis= input("kinte 4to nibud: (bumaga,nosznitsi,kamen)")
-a= x[randint(0,2)]
-print(a)
-if kis==a:
-    print("ni4ja")
+x=["kamen", "bumaga", "nosznitsi"]
+win=[("kamen","nosznitsi"),("bumaga","kamen"),("nosznitsi","bumaga")]
+xy=0
+xy1=0
+xy2=0
+game =input("robot või player: ")
+while game!="robot" and game!="player":
+    game =input("robot või player: ")
+if game=="robot":
+    for i in range(1,4):
+        robot = x[randint(0,2)]
+        y=input("choose kamen, bumaga, nosznitsi : ")
+        while y not in x:
+            y=input("choose kamen, bumaga, nosznitsi : ")
+        if y == robot:
+            print("ni4ja")
+            xy+=1
+        elif (y,robot) in win:
+            print("ti win")
+            xy1+=1
+        else:
+            print("you proigral")
+            xy2+=1
+    print(f"ni4ja: {xy} , you win: {xy1}, you proigral: {xy2}")
+elif game=="player":
+    for i in range(1,4):
+        print(f"{i} game")
+        y=input("Player1 choose kamen, bumaga, nosznitsi : ")
+        while y not in x:
+            y=input("Player1 choose kamen, bumaga, nosznitsi : ")
+        player=input("Player2 choose kamen, bumaga, nosznitsi : ")
+        while player not in x:
+            player=input("Player2 choose kamen, bumaga, nosznitsi : ")
+        if y == player:
+            print("ni4ja")
+            xy+=1
+        elif (y,player) in win:
+            print("player 1 win")
+            xy1+=1
+        else:
+            print("player 2 win")
+            xy2+=1
+    print(f"ni4ja: {xy} , player1 win: {xy1}, player2 win: {xy2}")
